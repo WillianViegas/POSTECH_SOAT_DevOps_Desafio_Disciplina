f6e4c34cfcda7346501f11664f61d7d7a1b250ff
SANMY RODRIGUES <145683987+sanmyrodri@users.noreply.github.com>
2024-02-22 21:11:31 -0300

    Update DataApplication.java

--- src/main/java/com/dev/cicd/data/DataApplication.java
+++ src/main/java/com/dev/cicd/data/DataApplication.java
@@ -3,6 +3,9 @@ package com.dev.cicd.data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//teste
+
 @SpringBootApplication
 public class DataApplication {
 
